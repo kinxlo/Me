a0ca3344e4164de193560198d7435564ed1a3799
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-21 08:12:07 +0100

    init: welcome

--- src/lib/react-query/query-keys.ts
+++ src/lib/react-query/query-keys.ts
@@ -1,8 +1,1 @@
-export const queryKeys = {
-  product: {
-    list: () => ["product", "list"] as const,
-    details: (id: string) => ["product", "details", id] as const,
-    filtered: (filters: IFilters) => ["product", "filtered", filters] as const,
-  },
-  // Add other domains as needed
-};
+export {};
