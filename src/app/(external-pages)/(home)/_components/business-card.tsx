0ed7c26de7ad53b1905644472faef3ac351e5723
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-27 08:13:43 +0100

    fix: cleanup

--- src/app/(external-pages)/(home)/_components/business-card.tsx
+++ src/app/(external-pages)/(home)/_components/business-card.tsx
@@ -6,27 +6,27 @@ import { PlainCard } from "./plain-card";
 
 export const BusinessCard = () => {
   return (
-    <PlainCard className="bg-foreground text-background space-y-8 p-10">
+    <PlainCard className="bg-foreground text-background space-y-8 p-5 lg:p-10">
       <section className={`space-y-4`}>
-        <p className={`font-mono text-sm uppercase`}>Get everything with</p>
-        <h3>
+        <p className={`font-mono text-xs uppercase lg:text-sm`}>Get everything with</p>
+        <h3 className={`text-xl lg:text-3xl`}>
           <span className={`text-primary`}>Ifijeh</span> Kingsley Solomon
         </h3>
       </section>
       <section className={`space-y-4`}>
-        <div className={`flex items-center justify-between space-y-2`}>
-          <p className={`font-mono text-4xl font-black tracking-tighter`}>~3 Years</p>
+        <div className={`flex items-center justify-between gap-2`}>
+          <p className={`font-mono text-xl font-black tracking-tighter lg:text-3xl`}>~3 Years</p>
           <MainButton variant={`primary`} className={`rounded-full`}>
             View Resume
           </MainButton>
         </div>
-        <p>
+        <p className={`text-xs/4 lg:text-[16px]/6`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa suscipit recusandae qui dolore at ullam
           corrupti laborum officiis ducimus? Ratione id quisquam consectetur neque soluta amet ad eveniet ipsa dolorum.
         </p>
       </section>
       <Separator className={`bg-gray-100/10`} />
-      <section className={`space-y-4`}>
+      <section className={`space-y-4 text-xs/4 lg:text-[16px]/6`}>
         <div className={`flex items-start gap-4`}>
           <span>
             <CheckCircle size={20} className={`text-success`} />
@@ -55,7 +55,7 @@ export const BusinessCard = () => {
           </p>
         </div>
       </section>
-      <div className={`bg-secondary/20 flex gap-4 rounded-xl p-4`}>
+      <div className={`bg-secondary/20 flex gap-4 rounded-xl p-4 text-xs/4 lg:text-[16px]/6`}>
         <span>
           <Phone size={20} className={`text-success`} />
         </span>
