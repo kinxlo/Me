b838064ec808f732b520824bf00c848ec0a0ecc0
Kingsley Ifijeh <kinxly@gmail.com>
2025-06-21 18:15:26 +0100

    fix:add scroll trigger

--- src/components/shared/navbar/mobile-menu-button.tsx
+++ src/components/shared/navbar/mobile-menu-button.tsx
@@ -11,7 +11,7 @@ export const MobileMenuButton = ({ isOpen, ...properties }: MobileMenuButtonProp
   return (
     <button
       {...properties}
-      className="fixed top-4 right-4 z-[1000] mr-1 rounded-full border-2 border-black bg-black/10 p-2 transition-all hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20"
+      className="fixed top-4 right-4 z-[1000] mr-1 rounded-full border-2 border-black bg-black/10 p-2 backdrop-blur-3xl transition-all hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20"
       aria-label={isOpen ? "Close menu" : "Open menu"}
     >
       <div className={cn("h-0.5 w-6 bg-black transition-all", isOpen && "translate-y-1.5 rotate-45")} />
